741f9697cdf39a2ddf9557ec552b6b4f1ddc465f
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-03-28 20:24:03 +0300

    message6

--- lab4/Example1.cpp
+++ lab4/Example1.cpp
@@ -4,6 +4,8 @@
 // strcpy() - копирование строк
 // strcmp() - сравнение строк
 // Для работы с этими функциями подключить файл string.h
+#include <iostream>
+#include <string.h>
 
 int main()
 {
@@ -13,11 +15,24 @@ int main()
   char str3[20];
   int len;
 
-  // использование strlen()
+  // использование strlen() для определения длины строки
   len = strlen(str1);
-  std::cout << "Длина строки '" << str1 << "'равена " << len << std::endl;
+  std::cout << "Длина строки '" << str1 << "'равна " << len << std::endl;
 
-  // использование stract()
-  stract(str1, str2);
+  // использование stract() для конкатенации строк
+  strcat(str1, str2);
   std::cout << "Строка после объединения: " << str1 << std::endl;
+
+  // использование strepy() - копирование строк
+  strcpy(str2, str1);
+  std::cout << "после копирования " << str2 << std::endl;
+
+  // использование strcmp() - сравнение строк
+  if (strcmp(str2, str1) == 0)
+  {
+    std::cout << "слова равны" << std::endl;
+  }
+  else
+    std::cout << "Слова не равны" << std::endl;
+  return 0;
 }
